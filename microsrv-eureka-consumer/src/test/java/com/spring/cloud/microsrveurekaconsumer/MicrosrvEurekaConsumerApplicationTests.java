cca06463200cd47d042277989f04c9371dd0b08a
ytzhou <ytzhou@homeinns.com>
2018-06-13 22:58:04 +0800

    update eureka service provider

---
+++ microsrv-eureka-consumer/src/test/java/com/spring/cloud/microsrveurekaconsumer/MicrosrvEurekaConsumerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spring.cloud.microsrveurekaconsumer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicrosrvEurekaConsumerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
