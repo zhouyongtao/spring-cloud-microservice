b0e01ab2b1fe3327ee86453114f8c7ddf5f82977
ytzhou <ytzhou@homeinns.com>
2018-06-13 23:48:34 +0800

    update microsrv-eureka-service-consumer

---
+++ microsrv-eureka-service-consumer/src/main/java/com/spring/cloud/microsrveurekaserviceconsumer/MicrosrvEurekaServiceConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.spring.cloud.microsrveurekaserviceconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosrvEurekaServiceConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrosrvEurekaServiceConsumerApplication.class, args);
+    }
+}
