dcbbb67f51b3d84f9c08eb24dcdc177ba3d6423f
ytzhou <ytzhou@homeinns.com>
2018-06-13 17:20:28 +0800

    update microsrv-hystrix-dashboard

---
+++ microsrv-hystrix-dashboard/src/main/java/com/spring/cloud/microsrvhystrixdashboard/MicrosrvHystrixDashboardApplication.java
@@ -0,0 +1,12 @@
+package com.spring.cloud.microsrvhystrixdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosrvHystrixDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrosrvHystrixDashboardApplication.class, args);
+    }
+}
---
+++ microsrv-hystrix-dashboard/src/test/java/com/spring/cloud/microsrvhystrixdashboard/MicrosrvHystrixDashboardApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spring.cloud.microsrvhystrixdashboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicrosrvHystrixDashboardApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
