1e39331c0bf075e5ed92ab69d61dccce2b882a20
ytzhou <ytzhou@homeinns.com>
2018-06-27 17:45:55 +0800

    remove spring-boot-starter-security

---
+++ microsrv-eureka-server/src/main/java/com/spring/cloud/microsrveurekaserver/WebSecurityConfig.java
@@ -0,0 +1,14 @@
+//package com.spring.cloud.microsrveurekaserver;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//@EnableWebSecurity
+//class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().ignoringAntMatchers("/eureka/**");
+//        super.configure(http);
+//    }
+//}
