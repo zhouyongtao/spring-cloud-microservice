381e3d6b0efef8bba160053112b264dde6eb5710
ytzhou <ytzhou@homeinns.com>
2018-07-02 17:47:47 +0800

    ## IP 的方式

--- microsrv-eureka-service-provider/src/main/java/com/spring/cloud/microsrveurekaserviceprovider/MicrosrvEurekaServiceProviderApplication.java
+++ microsrv-eureka-service-provider/src/main/java/com/spring/cloud/microsrveurekaserviceprovider/MicrosrvEurekaServiceProviderApplication.java
@@ -3,8 +3,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MicrosrvEurekaServiceProviderApplication {
 
     public static void main(String[] args) {
