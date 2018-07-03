dca4205bae843546009c1ca4d455dbbecac17e29
ytzhou <ytzhou@homeinns.com>
2018-07-03 16:02:54 +0800

    测试环境关闭自我保护模式

--- microsrv-zuul/src/main/java/com/spring/cloud/microsrvzuul/MicrosrvZuulApplication.java
+++ microsrv-zuul/src/main/java/com/spring/cloud/microsrvzuul/MicrosrvZuulApplication.java
@@ -1,11 +1,11 @@
 package com.spring.cloud.microsrvzuul;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class MicrosrvZuulApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MicrosrvZuulApplication.class, args);
     }
