480ab6eaa653f9715fdb70ebf6bab2df3f597321
ytzhou <ytzhou@homeinns.com>
2018-07-03 17:36:35 +0800

    MicrosrvZuulGatewayApplication

--- microsrv-zuul-gateway/src/main/java/com/spring/cloud/microsrvzuul/MicrosrvZuulGatewayApplication.java
+++ microsrv-zuul-gateway/src/main/java/com/spring/cloud/microsrvzuul/MicrosrvZuulGatewayApplication.java
@@ -3,7 +3,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-
 @SpringBootApplication
 @EnableZuulProxy
 public class MicrosrvZuulGatewayApplication {
