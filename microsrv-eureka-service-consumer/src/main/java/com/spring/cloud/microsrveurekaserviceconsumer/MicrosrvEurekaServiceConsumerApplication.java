2b4a04838c5f6e2e816a191fe94e363aea21fc4c
ytzhou <ytzhou@homeinns.com>
2018-07-05 11:20:47 +0800

    md orderService

--- microsrv-eureka-service-consumer/src/main/java/com/spring/cloud/microsrveurekaserviceconsumer/MicrosrvEurekaServiceConsumerApplication.java
+++ microsrv-eureka-service-consumer/src/main/java/com/spring/cloud/microsrveurekaserviceconsumer/MicrosrvEurekaServiceConsumerApplication.java
@@ -1,8 +1,9 @@
 package com.spring.cloud.microsrveurekaserviceconsumer;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 
+@EnableCircuitBreaker
 @SpringBootApplication
 public class MicrosrvEurekaServiceConsumerApplication {
 
