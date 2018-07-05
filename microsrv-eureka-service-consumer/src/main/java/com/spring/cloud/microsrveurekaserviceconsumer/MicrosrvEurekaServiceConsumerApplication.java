97cb2307cac4bbdb013c14e45ab7b268aece18ed
ytzhou <ytzhou@homeinns.com>
2018-07-05 14:41:59 +0800

    md microsrv-sleuth-zipkin-server

--- microsrv-eureka-service-consumer/src/main/java/com/spring/cloud/microsrveurekaserviceconsumer/MicrosrvEurekaServiceConsumerApplication.java
+++ microsrv-eureka-service-consumer/src/main/java/com/spring/cloud/microsrveurekaserviceconsumer/MicrosrvEurekaServiceConsumerApplication.java
@@ -1,13 +1,30 @@
 package com.spring.cloud.microsrveurekaserviceconsumer;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.stereotype.Component;
+import java.util.Map;
 
-@EnableCircuitBreaker
 @SpringBootApplication
+@EnableCircuitBreaker
 public class MicrosrvEurekaServiceConsumerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MicrosrvEurekaServiceConsumerApplication.class, args);
+    }
+
+    @Component
+    public class StoreIntegration {
+
+        @HystrixCommand(fallbackMethod = "defaultName")
+        public Object getStores(Map<String, Object> parameters) {
+            //do stuff that might fail\
+            return  "irving";
+        }
+
+        public Object defaultName(Map<String, Object> parameters) {
+            return "no name";
+        }
     }
 }
--- microsrv-eureka-service-provider/src/main/java/com/spring/cloud/microsrveurekaserviceprovider/MicrosrvEurekaServiceProviderApplication.java
+++ microsrv-eureka-service-provider/src/main/java/com/spring/cloud/microsrveurekaserviceprovider/MicrosrvEurekaServiceProviderApplication.java
@@ -2,10 +2,18 @@ package com.spring.cloud.microsrveurekaserviceprovider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RestController
 public class MicrosrvEurekaServiceProviderApplication {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World!";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MicrosrvEurekaServiceProviderApplication.class, args);
