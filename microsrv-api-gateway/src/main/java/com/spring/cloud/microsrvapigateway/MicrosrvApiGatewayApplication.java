87b3948019b75d7caf51753d72bc846ade8214fc
ytzhou <ytzhou@homeinns.com>
2018-07-05 16:41:45 +0800

    enable spring.cloud.gateway.discovery.locator.enabled

--- microsrv-api-gateway/src/main/java/com/spring/cloud/microsrvapigateway/MicrosrvApiGatewayApplication.java
+++ microsrv-api-gateway/src/main/java/com/spring/cloud/microsrvapigateway/MicrosrvApiGatewayApplication.java
@@ -1,11 +1,8 @@
 package com.spring.cloud.microsrvapigateway;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class MicrosrvApiGatewayApplication {
 
     public static void main(String[] args) {
