328d630e8588efa26ce72b863f2932409985032d
ytzhou <ytzhou@homeinns.com>
2018-07-05 10:30:54 +0800

    udpate microsrv-spring-boot-admin-server

---
+++ microsrv-spring-boot-admin-server/src/main/java/com/spring/cloud/microsrvspringbootadminserver/MicrosrvSpringBootAdminServerApplication.java
@@ -0,0 +1,20 @@
+package com.spring.cloud.microsrvspringbootadminserver;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+//http://codecentric.github.io/spring-boot-admin/2.0.1/
+
+@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@EnableAdminServer
+public class MicrosrvSpringBootAdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrosrvSpringBootAdminServerApplication.class, args);
+    }
+}
---
+++ microsrv-spring-boot-admin-server/src/test/java/com/spring/cloud/microsrvspringbootadminserver/MicrosrvSpringBootAdminServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spring.cloud.microsrvspringbootadminserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicrosrvSpringBootAdminServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
