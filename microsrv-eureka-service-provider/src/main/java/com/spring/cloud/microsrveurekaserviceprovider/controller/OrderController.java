6f09ddeebf788a09d6a8bae013e4358ab3de048d
ytzhou <ytzhou@homeinns.com>
2018-07-05 11:02:38 +0800

    md orderService

---
+++ microsrv-eureka-service-provider/src/main/java/com/spring/cloud/microsrveurekaserviceprovider/controller/OrderController.java
@@ -0,0 +1,32 @@
+package com.spring.cloud.microsrveurekaserviceprovider.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+/**
+ * 订单控制器
+ */
+@RestController
+@RequestMapping("/v1/order")
+public class OrderController {
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @RequestMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+            @PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
+    }
+
+    @RequestMapping("/total")
+    public Integer getTotalNum()
+    {
+        return 100;
+    }
+
+}
