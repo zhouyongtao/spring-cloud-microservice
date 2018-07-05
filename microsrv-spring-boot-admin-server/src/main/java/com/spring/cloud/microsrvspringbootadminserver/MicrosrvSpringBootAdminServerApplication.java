e7624bb5f41f21e783b6f8b1eeabaa92e1b6806f
ytzhou <ytzhou@homeinns.com>
2018-07-05 10:38:50 +0800

    udpate microsrv-eureka-server

--- microsrv-spring-boot-admin-server/src/main/java/com/spring/cloud/microsrvspringbootadminserver/MicrosrvSpringBootAdminServerApplication.java
+++ microsrv-spring-boot-admin-server/src/main/java/com/spring/cloud/microsrvspringbootadminserver/MicrosrvSpringBootAdminServerApplication.java
@@ -2,7 +2,6 @@ package com.spring.cloud.microsrvspringbootadminserver;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
