9360ee62a7f3f614885a0eb49dee3cc8e2381be2
ytzhou <ytzhou@homeinns.com>
2018-07-12 11:35:21 +0800

    update gateway hystrix

--- microsrv-zuul-gateway/src/main/java/com/spring/cloud/microsrvzuulgateway/MicrosrvZuulGatewayApplication.java
+++ microsrv-zuul-gateway/src/main/java/com/spring/cloud/microsrvzuulgateway/MicrosrvZuulGatewayApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
+@EnableResourceServer
 @EnableZuulProxy
 public class MicrosrvZuulGatewayApplication {
 
