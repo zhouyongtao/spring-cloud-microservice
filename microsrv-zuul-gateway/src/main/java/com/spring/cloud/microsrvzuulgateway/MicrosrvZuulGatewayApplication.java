361a0098a6cf1a0c8bd3831763e3c4be36396a43
ytzhou <ytzhou@homeinns.com>
2018-07-17 13:51:39 +0800

    md logging level

--- microsrv-zuul-gateway/src/main/java/com/spring/cloud/microsrvzuulgateway/MicrosrvZuulGatewayApplication.java
+++ microsrv-zuul-gateway/src/main/java/com/spring/cloud/microsrvzuulgateway/MicrosrvZuulGatewayApplication.java
@@ -3,11 +3,9 @@ import com.spring.cloud.microsrvzuulgateway.Filter.OAuth2TokenFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
-import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 
-//@SpringCloudApplication
 @SpringBootApplication
 @EnableOAuth2Sso
 @EnableZuulProxy
