7efd5de99a8d3c9d348c61e89d9c6635f347eadc
ytzhou <ytzhou@homeinns.com>
2018-08-26 11:52:18 +0800

    update spring cloud version Finchley.SR1

--- microsrv-oauth2-server/src/main/java/com/spring/cloud/microsrvoauth2server/controller/UserController.java
+++ microsrv-oauth2-server/src/main/java/com/spring/cloud/microsrvoauth2server/controller/UserController.java
@@ -1,14 +1,10 @@
 package com.spring.cloud.microsrvoauth2server.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import java.security.Principal;
-import java.util.HashMap;
-import java.util.Map;
 
 @RestController
 public class UserController {
@@ -16,21 +12,12 @@ public class UserController {
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @GetMapping("/userinfo")
-    public Principal user(Principal principal) {
+    public Principal user(Principal principal)
+    {
         return principal;
     }
 
-    @GetMapping("/me")
-    public Map<String, Object> me(OAuth2Authentication user) {
-        Map<String, Object> userInfo = new HashMap<String, Object>();
-        userInfo.put("user", user.getUserAuthentication().getPrincipal());
-        logger.debug("me:" + user.getUserAuthentication().getPrincipal().toString());
-        userInfo.put("authorities", AuthorityUtils.authorityListToSet(user.getUserAuthentication().getAuthorities()));
-        return userInfo;
-    }
-
-
-    @GetMapping("/{name}")
+    @GetMapping("/{name}/info")
     public String getBlogById(@PathVariable String name) {
         return "hello,"+ name;
     }
