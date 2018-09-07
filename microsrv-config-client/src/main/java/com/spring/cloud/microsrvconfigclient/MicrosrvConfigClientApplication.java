407682b3c25e4769b30a48860e33132cc76f532b
ytzhou <ytzhou@homeinns.com>
2018-09-07 21:23:21 +0800

    update config server with rabbitmq

--- microsrv-config-client/src/main/java/com/spring/cloud/microsrvconfigclient/MicrosrvConfigClientApplication.java
+++ microsrv-config-client/src/main/java/com/spring/cloud/microsrvconfigclient/MicrosrvConfigClientApplication.java
@@ -1,11 +1,23 @@
 package com.spring.cloud.microsrvconfigclient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RestController
 public class MicrosrvConfigClientApplication {
+
+    @Value("${config.evn}")
+    private String evn;
+
+    @RequestMapping("/")
+    public String home() {
+        return evn;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(MicrosrvConfigClientApplication.class, args);
