15b03c811567c970ef1d90009dd6482c16bb5b8a
ytzhou <ytzhou@homeinns.com>
2018-10-15 10:33:09 +0800

    md flowable

---
+++ microsrv-flowable/src/main/java/com/spring/cloud/microsrvflowable/MicrosrvFlowableApplication.java
@@ -0,0 +1,12 @@
+package com.spring.cloud.microsrvflowable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosrvFlowableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrosrvFlowableApplication.class, args);
+    }
+}
