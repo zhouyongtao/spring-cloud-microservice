06012a486d70d88ca21eb1cf8b39155f10881914
ytzhou <ytzhou@homeinns.com>
2018-10-15 14:12:41 +0800

    md flowable

--- microsrv-flowable/README.md
+++ microsrv-flowable/README.md
@@ -1,3 +1,8 @@
 ## docs
 * https://www.flowable.org/docs/userguide/index.html
 * https://github.com/flowable/flowable-engine
+
+## database
+```
+CREATE DATABASE `flowable` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+```
--- microsrv-flowable/src/main/java/com/spring/cloud/microsrvflowable/MicrosrvFlowableApplication.java
+++ microsrv-flowable/src/main/java/com/spring/cloud/microsrvflowable/MicrosrvFlowableApplication.java
@@ -1,12 +1,32 @@
 package com.spring.cloud.microsrvflowable;
 
+import org.flowable.engine.RepositoryService;
+import org.flowable.engine.RuntimeService;
+import org.flowable.engine.TaskService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MicrosrvFlowableApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MicrosrvFlowableApplication.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner init(final RepositoryService repositoryService,
+                                  final RuntimeService runtimeService,
+                                  final TaskService taskService) {
+
+        return f -> {
+            System.out.println("Number of process definitions : "
+                    + repositoryService.createProcessDefinitionQuery().count());
+            System.out.println("Number of tasks : " + taskService.createTaskQuery().count());
+            runtimeService.startProcessInstanceByKey("oneTaskProcess");
+            System.out.println("Number of tasks after process start: "
+                    + taskService.createTaskQuery().count());
+        };
     }
 }
