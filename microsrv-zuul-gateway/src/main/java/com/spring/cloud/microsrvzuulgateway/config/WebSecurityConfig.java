5e0b554e3eb34d1dfa66fb3eaa128749d01f2493
ytzhou <ytzhou@homeinns.com>
2018-10-18 13:20:53 +0800

    Update Spring Boot 2.0.6.RELEASE

--- microsrv-zuul-gateway/src/main/java/com/spring/cloud/microsrvzuulgateway/config/WebSecurityConfig.java
+++ microsrv-zuul-gateway/src/main/java/com/spring/cloud/microsrvzuulgateway/config/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.spring.cloud.microsrvzuulgateway;
+package com.spring.cloud.microsrvzuulgateway.config;
 //import org.springframework.context.annotation.Configuration;
 //import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 //import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
