72df6de0da11fe684eb47f942a0ab19d24846b4b
ytzhou <ytzhou@homeinns.com>
2018-10-18 13:30:51 +0800

    Update Spring Boot 2.0.6.RELEASE

--- microsrv-eureka-service-provider/src/main/java/com/spring/cloud/microsrveurekaserviceprovider/controller/OrderController.java
+++ microsrv-eureka-service-provider/src/main/java/com/spring/cloud/microsrveurekaserviceprovider/controller/OrderController.java
@@ -2,10 +2,13 @@ package com.spring.cloud.microsrveurekaserviceprovider.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
 import java.util.List;
 
 /**
@@ -30,4 +33,15 @@ public class OrderController {
         return 100;
     }
 
+    @GetMapping("/principal")
+    @PreAuthorize("hasAnyAuthority('user')")
+    public Principal user(Principal principal) {
+        return principal;
+    }
+
+    @GetMapping("/query")
+    @PreAuthorize("hasAnyAuthority('all')")
+    public String all () {
+        return "具有 all 权限";
+    }
 }
