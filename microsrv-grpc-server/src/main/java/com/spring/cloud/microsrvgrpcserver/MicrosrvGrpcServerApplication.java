cddc2d5674621bd4e96196960c3f1e8bbe88f3e8
ytzhou <ytzhou@homeinns.com>
2018-10-31 14:37:48 +0800

    Update Spring Boot 2.1.0.RELEASE

---
+++ microsrv-grpc-server/src/main/java/com/spring/cloud/microsrvgrpcserver/MicrosrvGrpcServerApplication.java
@@ -0,0 +1,12 @@
+package com.spring.cloud.microsrvgrpcserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosrvGrpcServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrosrvGrpcServerApplication.class, args);
+    }
+}
---
+++ microsrv-grpc-server/src/test/java/com/spring/cloud/microsrvgrpcserver/MicrosrvGrpcServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.spring.cloud.microsrvgrpcserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicrosrvGrpcServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
