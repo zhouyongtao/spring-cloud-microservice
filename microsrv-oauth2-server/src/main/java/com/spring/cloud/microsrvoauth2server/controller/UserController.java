8b16a433f7998fe73f18dbf404893dc01bae53e6
ytzhou <ytzhou@homeinns.com>
2018-11-28 16:24:38 +0800

    update readme.md

--- microsrv-oauth2-server/src/main/java/com/spring/cloud/microsrvoauth2server/controller/UserController.java
+++ microsrv-oauth2-server/src/main/java/com/spring/cloud/microsrvoauth2server/controller/UserController.java
@@ -3,15 +3,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.security.Principal;
 
 @RestController
+@RequestMapping("/api/v1/users")
 public class UserController {
 
     private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
-    @GetMapping("/userinfo")
+    @GetMapping("/me")
     public Principal user(Principal principal)
     {
         return principal;
