44b3bdc4be3608ebf14c60e0c43a5d48a327364a
ytzhou <ytzhou@homeinns.com>
2018-12-07 22:35:17 +0800

    add microsrv-oauth2-authorization-code

---
+++ microsrv-oauth2-authorization-code/src/main/java/com/spring/cloud/microsrvoauth2authorizationcode/MicrosrvOauth2AuthorizationCodeApplication.java
@@ -0,0 +1,12 @@
+package com.spring.cloud.microsrvoauth2authorizationcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosrvOauth2AuthorizationCodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrosrvOauth2AuthorizationCodeApplication.class, args);
+    }
+}
