43087bd9c5af9b93783672c88e78a34df40b1640
ytzhou <ytzhou@homeinns.com>
2018-12-13 13:44:44 +0800

    add nimbus-jose-jwt

---
+++ microsrv-spring-security-login/src/main/java/com/spring/cloud/microsrvspringsecuritylogin/MicrosrvSpringSecurityLoginApplication.java
@@ -0,0 +1,12 @@
+package com.spring.cloud.microsrvspringsecuritylogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosrvSpringSecurityLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrosrvSpringSecurityLoginApplication.class, args);
+    }
+}
